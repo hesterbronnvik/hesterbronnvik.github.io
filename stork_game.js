1f2ea3bc5ccf3ca49d13861fffce71a7fdc8cd10
Hester Brønnvik <80030806+hesterbronnvik@users.noreply.github.com>
2026-06-01 14:09:50 +0200

    Update stork_game.js

--- stork_game.js
+++ stork_game.js
@@ -1,7 +1,37 @@
-console.log("stork_game.js loaded");
+document.addEventListener("DOMContentLoaded", () => {
 
-const canvas = document.getElementById("gameCanvas");
-const ctx = canvas.getContext("2d");
+    console.log("stork_game.js loaded");
 
-ctx.fillStyle = "red";
-ctx.fillRect(50, 50, 100, 100);
+    const canvas = document.getElementById("gameCanvas");
+    const ctx = canvas.getContext("2d");
+
+    const player = {
+        x: 50,
+        y: 170,
+        width: 40,
+        height: 40
+    };
+
+    function draw() {
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // ground
+        ctx.beginPath();
+        ctx.moveTo(0, 210);
+        ctx.lineTo(canvas.width, 210);
+        ctx.stroke();
+
+        // stork
+        ctx.fillStyle = "#3a5a40";
+        ctx.fillRect(
+            player.x,
+            player.y,
+            player.width,
+            player.height
+        );
+    }
+
+    draw();
+
+});
